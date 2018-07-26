7d422f067e1e07c658d247db0596733c8ef59b29
Amithab <amiquest12@gmail.com>
2018-07-25 22:09:43 -0700

    started testing with junit

---
+++ TestJUnit.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class TestJUnit {
+  @Test
+  public void testSetup() {
+    String str = "I";
+    assertEquals("I", str);
+    assertEquals("A", "O");
+    assertEquals("B", "C");
+    System.out.println("Done testing");
+  }
+}
